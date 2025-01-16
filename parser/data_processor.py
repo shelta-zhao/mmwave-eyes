6a022014af3447acd1bbe02d783c994547ce818a
shelta-zhao <xiaotang_zhao@outlook.com>
2025-01-16 19:42:58 +0800

    feat: add doppler fft function

--- parser/data_processor.py
+++ parser/data_processor.py
@@ -8,11 +8,11 @@
 import os
 import yaml
 import torch
-import scipy
 import numpy as np
 import pandas as pd
 from param_generate import get_radar_params
 from data_loader import get_regular_data
+import matplotlib.pyplot as plt
 
 
 def adc_to_pcd(regular_data, radar_params, device, save=False, load=False):
@@ -32,24 +32,39 @@ def adc_to_pcd(regular_data, radar_params, device, save=False, load=False):
     else:
         # Perform range_fft
         range_fft_output = range_fft(regular_data, radar_params['rangeFFTObj'], device)
+        # Perfrom doppler fft
+        doppler_fft_out = doppler_fft(range_fft_output, radar_params['dopplerFFTObj'], device)
+
 
 
 def range_fft(input, rangeFFTObj, device):
     """
-    Range FFT
-    :param regular_data: regular data
-    :param rangeFFTObj: range FFT object
-    :return: range FFT data
+    Perform Range FFT on the input data.
+    
+    Parameters:
+        input (np.ndarray): The input data to be transformed.
+        rangeFFTObj (dict): A dictionary containing the parameters for the range FFT, including:
+            - 'radarPlatform' (str): The type of radar platform.
+            - 'rangeFFTSize' (int): The size of the FFT.
+            - 'dcOffsetCompEnable' (bool): Whether to enable DC offset compensation.
+            - 'rangeWindowEnable' (bool): Whether to enable range-domain windowing.
+            - 'FFTOutScaleOn' (bool): Whether to apply a scale factor to the FFT output.
+            - 'scaleFactorRange' (float): The scale factor to apply if enabled.
+        device (str): The device to perform the computation on ('cpu' or 'cuda').
+
+    Returns:
+        torch.Tensor: The range FFT result. Shape: (num_frames, range_fft_size, num_chirps, num_rx, num_tx)
     """
 
     # Get basic fft params
     radar_type, fft_size = rangeFFTObj['radarPlatform'], rangeFFTObj['rangeFFTSize']
-    dc_on, win_on, scale_on, scale_factor = rangeFFTObj['dcOffsetCompEnable'], rangeFFTObj['rangeWindowEnable'], rangeFFTObj['FFTOutScaleOn'], rangeFFTObj['scaleFactorRange']
+    dc_on, win_on = rangeFFTObj['dcOffsetCompEnable'], rangeFFTObj['rangeWindowEnable']
+    scale_on, scale_factor = rangeFFTObj['FFTOutScaleOn'], rangeFFTObj['scaleFactorRange']
 
-    # Convert input to tensor & Create output
+    # Convert input to tensorCreate output
     input = torch.tensor(input, dtype=torch.complex64).to(device)
+    # Generate window coefficient
     win_coeff = torch.tensor(np.hanning(input.shape[1] + 2)[1:-1], dtype=torch.float32).to(device)
-
     # Apply DC offset compensation
     input = input - torch.mean(input, dim=1, keepdim=True) if dc_on else input
     # Apply range-domain windowing
@@ -63,17 +78,40 @@ def range_fft(input, rangeFFTObj, device):
         fft_output[:, :, :, 1:3, :] *= torch.exp(-1j * torch.pi)
 
     # Return range fft result
-    return fft_output.cpu() if device == 'cuda' else fft_output 
+    return fft_output
 
-def doppler_fft(rangeFFT_output, dopplerFFTObj):
+def doppler_fft(input, dopplerFFTObj, device):
     """
-    Doppler FFT
-    :param rangeFFT_output: range FFT output
-    :param dopplerFFTObj: doppler FFT object
-    :return: doppler FFT data
+    Perform Doppler FFT on the input data (result of range fft).
+    
+    Parameters:
+        input (torch.Tensor): The input data to be transformed.
+        dopplerFFTObj (dict): A dictionary containing the parameters for the doppler FFT, including:
+            - 'dopplerFFTSize' (int): The size of the FFT.
+            - 'dopplerWindowEnable' (bool): Whether to enable doppler-domain windowing.
+            - 'FFTOutScaleOn' (bool): Whether to apply a scale factor to the FFT output.
+            - 'scaleFactorDoppler' (float): The scale factor to apply if enabled.
+        device (str): The device to perform the computation on ('cpu' or 'cuda').
+
+    Returns:
+        torch.Tensor: The doppler FFT result. Shape: (num_frames, range_fft_size, doppler_fft_size, num_rx, num_tx)
     """
-    doppler_fft_data = dopplerFFTObj.doppler_fft(rangeFFT_output)
-    return doppler_fft_data
+
+    # Get the basic fft parmas
+    fft_size, win_on  = dopplerFFTObj['dopplerFFTSize'], dopplerFFTObj['dopplerWindowEnable']
+    scale_on, scale_factor = dopplerFFTObj['FFTOutScaleOn'], dopplerFFTObj['scaleFactorDoppler']
+
+    # Generate window coefficient
+    win_coeff = torch.tensor(np.hanning(input.shape[1] + 2)[1:-1], dtype=torch.float32).to(device)
+    # Apply doppler-domain windowing
+    input = input * win_coeff.view(-1, 1, 1, 1) if win_on else input
+    # Perform FFT for each TX/RX chain
+    fft_output = torch.fft.fftshift(torch.fft.fft(input, n=fft_size, dim=2), dim=2)
+    # Apply scale factor
+    fft_output = fft_output * scale_factor if scale_on else fft_output
+
+    # Return doppler fft result
+    return fft_output.cpu() if device == 'cuda' else fft_output 
 
 def angle_fft(rangeFFT_output, DOAObj):
     """
@@ -103,6 +141,7 @@ if __name__ == "__main__":
         data = yaml.safe_load(file)
     data_path = os.path.join("datas/adcDatas", f"{data['prefix']}/{data['index']}")
     config_path = os.path.join("datas/configs", data["config"])
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     
     # Get radar params
     radar_params = get_radar_params(config_path, data['radar'], load=True)
@@ -111,5 +150,9 @@ if __name__ == "__main__":
     regular_data, timestamp = get_regular_data(data_path, radar_params['readObj'], '1', timestamp=True)
 
     # Test Range FFT
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     range_fft_output = range_fft(regular_data, radar_params['rangeFFTObj'], device)
+
+    # Test Doppler FFT
+    doppler_fft_out = doppler_fft(range_fft_output, radar_params['dopplerFFTObj'], device)
+
+
